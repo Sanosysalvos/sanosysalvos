9ea73302e3e93354b972d40a227f1b5fd7cd2423
sanosysalvos33@gmail.com <sanosysalvos33@gmail.com>
2026-05-16 01:20:36 -0400

    Revert "Actualización next config"
    This reverts commit 1dd13005f6d982731bdd01baa03675640f667220.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api-aws/:path*",
-        destination: "http://52.7.90.137/:path*", // Recuerda agregar el puerto al final si tu backend no usa el puerto 80 (ej: :3000)
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
