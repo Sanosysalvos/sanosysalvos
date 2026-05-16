1dd13005f6d982731bdd01baa03675640f667220
sanosysalvos33@gmail.com <sanosysalvos33@gmail.com>
2026-05-15 23:50:32 -0400

    Actualización next config
    Se agrega para evitar error de certificado mixto

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api-aws/:path*",
+        destination: "http://52.7.90.137/:path*", // Recuerda agregar el puerto al final si tu backend no usa el puerto 80 (ej: :3000)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
